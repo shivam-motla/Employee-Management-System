641e5c769d9a74922b69817943d085652fdf2e25
shivam <shivammotla09@gmail.com>
2022-02-13 14:19:26 +0530

    Add project files.

---
+++ FirstMVCApplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstMVCApplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
