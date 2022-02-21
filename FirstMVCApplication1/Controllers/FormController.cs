07ddd09c97718ced377f2a007f6d243d30f9aeff
shivam <shivammotla09@gmail.com>
2022-02-21 19:28:59 +0530

    com

--- FirstMVCApplication1/Controllers/FormController.cs
+++ FirstMVCApplication1/Controllers/FormController.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using FirstMVCApplication1.Data;
+using FirstMVCApplication1.Models;
+
 namespace FirstMVCApplication1.Controllers
 {
     public class FormController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public FormController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
+
         }
-        public IActionResult ViewData() 
-        { 
-            return View();
+        public IActionResult ShowData() 
+        {
+            IEnumerable<AddAddress> obj_addressinfo_list = _db.AddAddresses;
+            return View(obj_addressinfo_list);
         }
     }
 }
